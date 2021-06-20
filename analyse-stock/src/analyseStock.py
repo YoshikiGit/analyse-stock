6a0724b28c05ac8d0f819eced7a166f67c2a076a
佐藤利紀 <ysklove.music1stsight@gmail.com>
2021-06-20 21:30:46 +0900

    ROEを参照するように条件追加

--- analyse-stock/src/analyseStock.py
+++ analyse-stock/src/analyseStock.py
@@ -5,6 +5,7 @@ import requests
 import time
 from dataclasses import dataclass
 import pyperclip
+import tkinter as tk
 from tkinter import messagebox
 
 
@@ -24,38 +25,49 @@ class StockInfo:
 class Main():
     # 値が設定されていなければskip
     def hyphen_check(self, targetStr):
-        print("check")
         if ("-" in targetStr):
             return False;
         
         return True;
     # /hyphen_check
     
+    def shape_format(self, targetStr):
+        shapedStr = targetStr.rstrip("倍");
+        shapedStr = shapedStr.replace(',', '');
+        return float(shapedStr);
+    
     
     # 株をフィルタ
     def set_condition(self, basic_info, keyList):
-        print(keyList)
-        print(basic_info)
+        #print(keyList)
         
         # PER
         if self.hyphen_check(basic_info[keyList[5]]):
-            if float(basic_info[keyList[5]].rstrip("倍")) > 15:
-                print("PER OUT")
+            if self.shape_format(basic_info[keyList[5]]) > 15:
+                print("×PER OUT")
                 return False
+        else:
+            return False
                     
         # PBR
         if self.hyphen_check(basic_info[keyList[7]]):
-            if float(basic_info[keyList[7]].rstrip("倍")) > 1:
-                print("PBR OUT")
+            if self.shape_format(basic_info[keyList[7]]) > 1:
+                print("×PBR OUT")
                 return False
         
                             
-        # 時価総額
+        # 時価総額（300億以下）
         if self.hyphen_check(basic_info[keyList[9]]):
             if float(basic_info[keyList[9]].rstrip("百万円").replace(',', '')) > 30000:
-                print("時価総額 OUT")
+                print("×時価総額 OUT")
                 return False
-                
+            
+        # ROE
+        roe = self.shape_format(basic_info[keyList[7]]) / self.shape_format(basic_info[keyList[5]])
+        if roe < 10 or 20 < roe:
+            return False
+        
+        print("Good")
         return True
             
     # /set_condition
@@ -64,6 +76,8 @@ main = Main()
 
 # みん株URL
 MINKABU_URL = "https://minkabu.jp/stock/"
+CHANGE_LINE = '''
+'''
     
 try:
     # 銘柄コードの取得
@@ -77,22 +91,21 @@ try:
     ## 各市場を整理
     #firstSectionCodeList = codelist.loc[codelist["市場・商品区分"] == "市場第一部（内国株）"]
     #secondSectionCodeList = codelist.loc[codelist["市場・商品区分"] == "市場第二部（内国株）"]
-    mothersCodeList = codelist.loc[codelist["市場・商品区分"] == "マザーズ（内国株）"]
-    print("===マザーズ（内国株） 母数 ===")
-    print(len(mothersCodeList))
-    jasdaqStandardCodeList = codelist.loc[codelist["市場・商品区分"] == "JASDAQ(スタンダード・内国株）"]
+    #mothersCodeList = codelist.loc[codelist["市場・商品区分"] == "マザーズ（内国株）"]
+    #print("===マザーズ（内国株） 母数 ===")
+    #print(len(mothersCodeList))
     print("=========================")
-        
     print("=== JASDAQ(スタンダード・内国株） 母数 ===")
-    print(len(jasdaqStandardCodeList))
-    jasdaqGrowthCodeList = codelist.loc[codelist["市場・商品区分"] == "JASDAQ(グロース・内国株）"]
+    #jasdaqStandardCodeList = codelist.loc[codelist["市場・商品区分"] == "JASDAQ(スタンダード・内国株）"]            
+    mothersCodeList = codelist.loc[codelist["市場・商品区分"] == "JASDAQ(スタンダード・内国株）"]            
+    print(len(mothersCodeList))
     print("====================================")
     print("=== JASDAQ(グロース・内国株 母数） ===")
+    jasdaqGrowthCodeList = codelist.loc[codelist["市場・商品区分"] == "JASDAQ(グロース・内国株）"]
     print(len(jasdaqGrowthCodeList))
     print("=================================")
 
-    print('''
-          ''')
+    print(CHANGE_LINE)
         
     ## TOPIXを整理
     #topix100CodeList = codelist.loc[codelist["規模区分"].isin([ "TOPIX Core30" ,  "TOPIX Large70" ])]
@@ -109,18 +122,20 @@ try:
             
         url = MINKABU_URL + str(mothersCodeList.iloc[tmpIndex,1])
             
-        if tmpIndex == 0 or tmpIndex == 1 or tmpIndex == 2 or tmpIndex == 3:
-            print ("アクセス先URL")
+        if 0 <= tmpIndex <= 600:
+            print ("■アクセス先URL")
             print (url)
+            print(CHANGE_LINE)
                 
             # 1s遅延でアクセス
-            time.sleep(1)
+            time.sleep(2)
             html = requests.get(url)
                 
             # BeautifulSoupのHTMLパーサーを生成
             soup = BeautifulSoup(html.content, "html.parser")
                 
             # データ格納用のディクショナリを準備
+            
             basic_info = {}
             keyList = {}
                 
@@ -146,7 +161,7 @@ try:
                 #print(key)
                 value = dd.text
                     
-                # ディクショナリに格納
+                # TOP情報をディクショナリに格納
                 basic_info[key] = value
                     
                 listIndex = listIndex + 1
@@ -157,18 +172,18 @@ try:
 
                     
             tmpIndex = tmpIndex + 1
-            
-    print("===== all_info =====")
-    print(all_info)
-        
-    if len(all_info) != 0: 
-        # クリップボードへコピー
-        pyperclip.copy('python')
-        messagebox.showinfo("完了通知", "クリップボードへ気になる株をコピーしました。")
+            print(CHANGE_LINE)
+                        
+
+    if len(all_info) != 0:
+        copy_text = ""
+        print("完了しました")
+        print("===== all_info =====")
+        print(all_info)
             
     else:
-        messagebox.showinfo("完了通知", "候補が存在しませんでした。")
-
+        print("候補が存在しませんでした。")
+    
 except Exception as ex:
     print(ex)
     
